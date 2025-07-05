3bd4c1ba853860b905b355823119ad9e5b98eda6
iseufsif <lucca.altendeitering@gmail.com>
2025-07-05 14:37:13 +0200

    implemented simulated annealing

--- heuristics/metaheuristics/diversifying_components/simulated_annealing.py
+++ heuristics/metaheuristics/diversifying_components/simulated_annealing.py
@@ -1,1 +1,49 @@
 # Simulated Annealing
+from utils.utils import compute_total_cost
+import numpy as np
+import time
+from heuristics.metaheuristics.neighborhood_operators.two_opt import two_opt_move
+import math
+
+def simulated_annealing(instance, routes, it=50, max_no_improvement=200, alpha=0.1, beta=0.9):
+    # Initialize
+    current_sol = routes.copy()
+    current_length = compute_total_cost(current_sol, instance["edge_weight"])
+    best_sol = current_sol
+    best_length = current_length
+
+    temperature = alpha * current_length
+    cooling = beta
+
+    # Set seed to reproduce random based results
+    # np.random.seed(1)
+
+    # Start LS
+    # start_time = time.time()
+    no_improv = 0
+    while no_improv < max_no_improvement:
+        improv = False
+        for r_idx in range(len(current_sol)):
+            for _ in range(it):
+                neighbor = two_opt_move(current_sol, r_idx)
+                neighbor_length = compute_total_cost(neighbor, instance["edge_weight"])
+                delta = neighbor_length - current_length
+                if delta < 0:
+                    improv = True
+                    current_sol = neighbor.copy()
+                    current_length = neighbor_length
+                    if current_length < best_length:
+                        best_sol = neighbor.copy()
+                        best_length = neighbor_length
+                        print(f"Route {r_idx}: New best solution value: {best_length}")
+                elif np.random.random() <  math.exp(-delta / temperature):
+                    current_sol = neighbor.copy()
+                    current_length = neighbor_length
+        
+        if improv:
+            no_improv = 0
+        else:
+            no_improv += 1
+        temperature = temperature * cooling
+
+    return best_sol
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from heuristics.construction.random import generate_random_solution
 from utils.utils import compute_total_cost, write_solution, print_solution
 from utils.plot import plot_routes
 from heuristics.metaheuristics.instensifying_components.ls import ls_with_2opt, ls_with_swaps
+from heuristics.metaheuristics.diversifying_components.simulated_annealing import simulated_annealing
 
 def main():
     random.seed(42)
@@ -12,22 +13,35 @@ def main():
     instance_name = "A-n45-k6.vrp"
 
     # Generate random solution
+    print("--- Randomized Solution ---")
     routes, instance = generate_random_solution(instance_name)
     cost = compute_total_cost(routes, instance["edge_weight"])
     print_solution(routes, cost)
+    print("---------------------------")
 
     # Call LS with Swaps
+    print("--- Solution LS #1 ---")
     routes_ls = ls_with_swaps(instance, routes, 1000)
     cost_ls = compute_total_cost(routes_ls, instance["edge_weight"])
     print_solution(routes_ls, cost_ls)
+    print("----------------------")
 
     # Call LS with 2-Opt
+    print("--- Solution LS #2 ---")
     routes_ls_2 = ls_with_2opt(instance, routes_ls, 1000)
     cost_ls_2 = compute_total_cost(routes_ls_2, instance["edge_weight"])
     print_solution(routes_ls_2, cost_ls_2)
+    print("----------------------")
+
+    # Call Simulated Annealing
+    print("--- Solution SA ---")
+    routes_sa = simulated_annealing(instance, routes_ls_2)
+    cost_sa = compute_total_cost(routes_sa, instance["edge_weight"])
+    print_solution(routes_sa, cost_sa)
+    print("-------------------")
 
     # Plot final result
-    plot_routes(instance, routes_ls_2, title=f"Random Solution for {instance_name}")
+    plot_routes(instance, routes_sa, title=f"Random Solution for {instance_name}")
 
 if __name__ == "__main__":
     main()
